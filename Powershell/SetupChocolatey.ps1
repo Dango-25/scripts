7162b2ecd60e52bca2499452263543735780f6b4
Daniel Gómez <gomezar20@gmail.com>
2021-09-08 22:45:37 -0500

    Update SetupChocolatey.ps1

--- Powershell/SetupChocolatey.ps1
+++ Powershell/SetupChocolatey.ps1
@@ -1,4 +1,5 @@
 # * Powershell Execution Policy bypassed to run these scripts
 Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
 
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
